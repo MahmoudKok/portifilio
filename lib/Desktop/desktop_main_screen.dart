870c3e79e222fb2c67fd9d620bc1bb93aa1f8110
Koooook39 <mahmoudkokeh462@gmail.com>
2023-09-28 10:17:54 +0300

    fix: Responsive for mobile and tablet

--- lib/Desktop/desktop_main_screen.dart
+++ lib/Desktop/desktop_main_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:portifilio/helpers/constants.dart';
 
+import 'components/aboutMe_section.dart';
+import 'components/contact_section.dart';
 import 'components/landing_section.dart';
+import 'components/myCodingKnowledge_sction.dart';
+import 'components/myExperince_section.dart';
+import 'components/myProjects_section.dart';
+import 'components/mySkills_section.dart';
 
 class DesktopMainScreen extends StatelessWidget {
   const DesktopMainScreen({super.key});
@@ -17,27 +23,27 @@ class DesktopMainScreen extends StatelessWidget {
           SizedBox(
             height: screenHeight * 0.05,
           ),
-          // AboutMeSection(),
-          // SizedBox(
-          //   height: screenHeight * 0.05,
-          // ),
-          // MySkillsSection(),
-          // SizedBox(
-          //   height: screenHeight * 0.05,
-          // ),
-          // MyCodingKnowledgeScreen(),
-          // SizedBox(
-          //   height: screenHeight * 0.05,
-          // ),
-          // MyExperincesScreen(),
-          // SizedBox(
-          //   height: screenHeight * 0.05,
-          // ),
-          // const MyProjectsScreen(),
-          // SizedBox(
-          //   height: screenHeight * 0.05,
-          // ),
-          // ContactSection(),
+          AboutMeSection(),
+          SizedBox(
+            height: screenHeight * 0.05,
+          ),
+          MySkillsSection(),
+          SizedBox(
+            height: screenHeight * 0.05,
+          ),
+          MyCodingKnowledgeScreen(),
+          SizedBox(
+            height: screenHeight * 0.05,
+          ),
+          MyExperincesScreen(),
+          SizedBox(
+            height: screenHeight * 0.05,
+          ),
+          MyProjectsScreen(),
+          SizedBox(
+            height: screenHeight * 0.05,
+          ),
+          ContactSection(),
         ],
       ),
     );
